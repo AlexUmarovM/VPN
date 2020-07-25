706f0da601d3be1df7525b06598c4258edf0c3e0
Alex Umarov <umarov.iosdev@gmail.com>
2020-07-25 16:11:41 +0300

    Initial Commit

---
+++ VPN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VPN
+//
+//  Created by Александр Умаров on 25.07.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
